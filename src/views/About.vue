7734aec1652e1bb81e4efd8e77f9820b5f680ec7
KeliTubin <Keli.Tubin@vikk.ee>
2021-11-07 18:57:41 +0200

    About update with Subpage settings

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h1>This is an about page</h1> -->
+    <Subpage title="SUBPAGE"/>
   </div>
 </template>
+
+<script>
+import Subpage from "@/components/Subpage.vue";
+export default {
+  name: "About",
+  components: {
+    Subpage,
+  },
+};
+</script>
