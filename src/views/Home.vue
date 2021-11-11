0f054e2a19e10f0b2286366959b2ba28f0f388e3
rometnaruskberg <91244913+rometnaruskberg@users.noreply.github.com>
2021-11-11 08:20:11 +0200

    Update Home.vue
    Adder header and footer code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,15 @@
 <script>
 // @ is an alias to /src
 import Calculate from "@/components/Calculate.vue";
+import footer from "@/components/footer.vue";
+import header from "@/components/header.vue";
 
 export default {
   name: "Home",
   components: {
     Calculate,
+    footer,
+    header,
   },
 };
 </script>
