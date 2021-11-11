57f111dd959d95d2bebb8535c5337fbe7d498c46
rometnaruskberg <91244913+rometnaruskberg@users.noreply.github.com>
2021-11-11 07:53:02 +0200

    Update footer.vue
    Updated footer code

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,13 @@
-<footer>
-    <div class="footerinfo"> 
-        Footer info here
-    </div>
-</footer>
+<template>             
+    <div>                   
+        <div class="row">                
+            <div class="col">            
+                <footer>
+                    <div class="footerinfo"> 
+                        Footer info here
+                    </div>
+                </footer>
+            </div> 
+        </div>   
+    </div> 
+</template>
