5d700ddf413fe68c8c7748a2979432576846dd9d
rometnaruskberg <91244913+rometnaruskberg@users.noreply.github.com>
2021-11-11 07:55:33 +0200

    Update header.vue
    Updated header code

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,3 +1,11 @@
-<header>
-    <img class="headerimage" src="headerpicture.jpg" alt="Logo">
-</header>
+<template>             
+    <div>                   
+        <div class="row">                
+            <div class="col">            
+                <header>
+                    <img class="headerimage" src="headerpicture.jpg" alt="Logo">
+                </header>
+            </div> 
+        </div>   
+    </div> 
+</template>
