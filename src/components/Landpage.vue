f8fbe25bc9ff81845d27119447fa8989f94cb723
KeliTubin <Keli.Tubin@vikk.ee>
2021-11-25 12:31:11 +0200

    Landingpage.vue and Searching.vue separated data

--- src/components/Landpage.vue
+++ src/components/Landpage.vue
@@ -5,11 +5,9 @@
       <div class="col-9">
         <ul class="list-group container">
           <li class="list-group-item" v-for="item in breeds" :key="item.Breed">
-
             <img :src="require(`./pics/${item.Breed}.jpg`)" alt="koera pilt" />
             <br />
             {{ item.Breed }}
- 
           </li>
         </ul>
       </div>
