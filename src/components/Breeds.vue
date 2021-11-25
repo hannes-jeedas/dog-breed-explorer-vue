33e43126862f546457bda5d64525a2d399e8a434
Janos Orgusaar <janos@vau.local>
2021-11-25 12:47:54 +0200

    landing page splitted

---
+++ src/components/Breeds.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Search -->
+  <div>
+
+      <div class="col-9">
+        <ul class="list-group container">
+          <li class="list-group-item" v-for="item in breeds" :key="item.Breed">
+
+            <img :src="require(`./pics/${item.Breed}.jpg`)" alt="koera pilt" />
+            <br />
+            {{ item.Breed }}
+ 
+          </li>
+        </ul>
+      </div>
+    
+  </div>
+</template>
+
+<script>
+import breedsData from "./breeds.json";
+export default {
+  data() {
+    return {
+      breeds: breedsData,
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="home">
     <Header msg="header" />
-    <Landpage msg="landpage" />
+    <div class="row">
+      <div class="col-2">
+    <Searching msg="Searching" />
+    </div>
+    <div class="col-10">
+    <Breeds msg="Breeds" />
+    </div>
+    </div>
     <Footer msg="footer" />
+    
   </div>
 </template>
 
 <script>
 import Header from "@/components/header.vue";
-import Landpage from "@/components/Landpage.vue";
+import Breeds from "@/components/Breeds.vue";
 import Footer from "@/components/footer.vue";
+import Searching from "@/components/Searching.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
-    Landpage,
+    Breeds,
     Footer,
+    Searching,
   },
 };
 </script>
