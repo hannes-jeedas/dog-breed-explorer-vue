62bdb984f7eac5a33e7a091504e5043200fc8a0a
Keli Tubin <keli.tubin>
2021-11-29 15:44:26 +0200

    Logo linked to Landing page

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,11 +3,13 @@
     <div class="row">
       <div class="col">
         <header>
-          <img
-            class="headerimage"
-            src="https://logos.textgiraffe.com/logos/logo-name/Jasmin-designstyle-pastel-m.png"
-            alt="Logo"
-          />
+          <router-link to="/">
+            <img
+              class="headerimage"
+              src="https://logos.textgiraffe.com/logos/logo-name/Jasmin-designstyle-pastel-m.png"
+              alt="Logo"
+            /> 
+          </router-link>
         </header>
       </div>
     </div>
