7c94afabcf49fb237246f8870b02abedb16c1104
Janos Orgusaar <janos@vau.local>
2021-12-01 12:57:50 +0200

    code update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="home">
     <Header msg="header" />
-    <Searching msg="searching"/>
-    <Landpage msg="landpage" />
+    <div class="row">
+      <div class="col-2 ">
+        <div class="container">
+          <Searching msg="Searching" />
+        </div>
+      </div>
+      <div class="col-9">
+        <Breeds msg="Breeds" />
+      </div>
+    </div>
     <Footer msg="footer" />
   </div>
 </template>
 
 <script>
 import Header from "@/components/header.vue";
-import Searching from "@/components/Searching.vue"
-import Landpage from "@/components/Landpage.vue";
+import Breeds from "@/components/Breeds.vue";
 import Footer from "@/components/footer.vue";
 import Searching from "@/components/Searching.vue";
 
@@ -18,8 +25,7 @@ export default {
   name: "Home",
   components: {
     Header,
-    Searching,
-    Landpage,
+    Breeds,
     Footer,
     Searching,
   },
