6b27f8db23dd1741e668c65e1f40561b7b26bb17
KeliTubin <Keli.Tubin@vikk.ee>
2021-12-01 14:42:25 +0200

    Feedback.vue draft 1

---
+++ src/components/Feedback.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h2>{{ title }}</h2>
+
+        <ul class="list-group mb-3">
+          <li
+            v-for="comment in commentssFromServer"
+            :key="comment"
+            class="list-group-item"
+            @click="getTodo(comment._id)"
+          >
+            {{ comment.title }} {{ comment.status }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6 commentbox">
+        <input
+          v-model="newComment"
+          type="text"
+          name="newComment"
+          class="form-control"
+        />
+      </div>
+      <div class="row">
+        <div class="col-2 submitcomment">
+            <button @click="addComment" type="submit" class="btn btn-primary w-100">
+            Submit
+            </button>
+            </div>
+        </div>
+    </div>
+    {{singleTodo}}
+  </div>
+</template>
+
+// <script>
+// import { ref } from "Jasmin";
+// import axios from "axios";
+
+// export default {
+//   name: "TodoList",
+//   props: {
+//     title: String,
+//   },
+
+//   setup() {
+//     const comments = ref(["Read a book", "Go for a walk", "Eat food"]);
+//     const newComment = ref("");
+//     const commentsFromServer = ref([]);
+//     const singleTodo = red({});
+
+//     async function deleteTodo() {
+//       await axios.get("/api/delete-todo" + id);
+//       await getComments();
+//     }
+
+//     async function completeTodo(id) {
+//       await axios.get("/api/update-todo" + id, {status: "COMPLETED"});
+//       await getComments();
+//     }
+
+
+
+//     async function getComments() {
+//       const result = await axios.get("/api/get-todos");
+//       commentsFromServer.value = result.data;  // axios data tuleb alati salvestada result.data //
+//       console.log(result.data);
+//     }
+
+//     async function getTodo(id) {
+//       const result = await axios.get("/api/get-todo/" + id);
+//       singleTodo.value = result.data;  // axios data tuleb alati salvestada result.data //
+//       console.log(result.data);
+//     }
+
+//     async function addComment(){
+//       await axios.post("/api/add/comment", {
+//         title: newComment.value,
+//         status: "ACTIVE",
+//       });
+//       newComment.value = "";
+//       await getComments(); // empty field after submit to todolist //
+//     }
+
+//     getComments();
+
+//     // function addNewTodo() {
+//     //   todos.value.push(newComment.value);
+//     //   newComment.value = "";
+//     // }
+
+//     return {
+//       comment,
+//       newComment,
+//       addNewTodo,
+//       commentFromServer,
+//       addComment,
+//       singleTodo,
+//       getTodo,
+//       deleteTodo,
+//       completeTodo,
+//     };
+//   },
+// };
+// </script>
+
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,7 @@
     <!-- <h1>This is an about page</h1> -->
     <Header title="header" />
     <Subpage title="Subpage" />
+    <Feedback title="Feedback" />
     <Footer title="footer" />
   </div>
 </template>
@@ -10,6 +11,7 @@
 <script>
 import Header from "@/components/header.vue";
 import Subpage from "@/components/subpage.vue";
+import Feedback from "@/components/Feedback.vue";
 import Footer from "@/components/footer.vue";
 
 export default {
@@ -17,6 +19,7 @@ export default {
   components: {
     Header,
     Subpage,
+    Feedback,
     Footer,
   },
 };
