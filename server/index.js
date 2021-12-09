a89e784e5818960031a500578a2844f27880f323
rometnaruskberg <91244913+rometnaruskberg@users.noreply.github.com>
2021-12-09 10:36:34 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const port = 4111
 const routes = require("./router")
 const bodyParser = require("body-parser");
 const db = require("./dbConnection");
+
+const expressJwt = require("express-jwt");
+const { SECRET } = require("./config");
+
+app.all( /* enne oli siin app.use*/
+  expressJwt({ secret: SECRET, algorithms: ["HS256"] }).unless({
+    path: ["/api/auth/register", "/api/auth/login"],
+  })
+);
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
